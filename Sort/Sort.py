4d886ec7caff052f561fd5a5f5ed18cac735e89c
“hyl2048” <2745506937@qq.com>
2022-04-17 11:08:18 +0800

    commit sort

--- Sort/Sort.py
+++ Sort/Sort.py
@@ -1,20 +1,23 @@
+# 边界
+    #
+# 异常值的考虑
 
-
-def bubble_sort(arr): #比较 冒泡
+# 时间复杂度
+def bubble_sort(arr): #比较 冒泡  时间复杂度 O(n^2)
     length = len(arr)
     for i in range(length):
-        for j in range(0,length-i-1):
+        for j in range(0,length-i-1): #每次把最小的放到最右边
               if arr[j] < arr[j+1]:
                   arr[j], arr[j+1] = arr[j+1], arr[j]
             # print(i)
     return arr
 
 def quick_sort(arr):# 快排 分治法 递归  1函数功能 2停止条件 3递归函数
-    if not len(arr):
+    if not len(arr): # 递归停止条件 和 异常考虑
         return []
     else:
-        pivot = arr[0]
-        left = quick_sort([x for x in arr[1:] if x >= pivot])
+        pivot = arr[0] #
+        left = quick_sort([x for x in arr[1:] if x >= pivot])# 递归函数与拆解
         right = quick_sort([x for x in arr[1:] if x < pivot])
     return left + [pivot] + right
 
@@ -46,7 +49,7 @@ def partition(arr, start, end):
     arr[start] = pivot
     return start
 
-def merge_sort(arr, l, r):
+def merge_sort(arr, l, r): #归并排序 2路归并
     if l < r:
         m = int((l + (r - 1))/2)
         merge_sort(arr, l, m)
@@ -54,37 +57,37 @@ def merge_sort(arr, l, r):
         merge(arr, l, m, r)
     # return arr
 
-def merge(arr, l, m, r):
-    n1 = m - l + 1
-    n2 = r - m
+def merge(arr, l, m, r): # 划分区间后左右按大小调序的操作，使用临时数组在原集合上操作
+    n1 = m - l + 1 # 左长
+    n2 = r - m # 右长
     
     L = [0] * (n1)
     R = [0] * (n2)
     
-    for i in range(0, n1):
+    for i in range(0, n1): # 初始化临时list
         L[i] = arr[l + i]
     
     for j in range(0, n2):
         R[j] = arr[m + 1 + j]
         
-    i = 0
+    i = 0 # 索引初始化
     j = 0
-    k = l
+    k = l # 初始化待调区间的索引
     
-    while i < n1 and j< n2:
-        if L[i] <= R[j]:
+    while i < n1 and j < n2: # 如何按大小顺序合并两个集合，
+        if L[i] <= R[j]: # 左边小的开始放在待调区间中
             arr[k] = L[i]
             i += 1
-        else:
+        else: # 反之右边小的开始放在待调区间中
             arr[k] = R[j]
             j += 1
         k += 1
-    while i < n1:
+    while i < n1: # 合并左边剩余未比较的
         arr[k] = L[i]
         i += 1
         k += 1
         
-    while j < n2:
+    while j < n2: #合并右边剩余未比较的
         arr[k] = R[j]
         j += 1
         k += 1
@@ -139,6 +142,10 @@ def merge(arr, l, m, r):
 #         mergeSort(arr, l, m)
 #         mergeSort(arr, m + 1, r)
 #         merge(arr, l, m, r)
+
+# 桶排序  计数排序的升级版
+
+# 堆排序 树的结构 大根堆 小根堆  选择排序的升级版
 
 
 
---
+++ Sort/note.md
@@ -0,0 +1,14 @@
+
+
+![sortType](C:\Users\Administrator\Pictures\note\algorithem\sortType.png)
+
+![Complex](C:\Users\Administrator\Pictures\note\algorithem\Complex.png)
+
+
+
+
+
+#### 参考：
+
+https://www.cnblogs.com/zhousong918/p/10172343.html
+
